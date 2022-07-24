aaf9ed72d1af85f6b26a062b608b5a01d9b5747b
mgood7123 <smallville7123@gmail.com>
2022-07-24 10:35:11 +1000

    add ArrayList

--- AndroidUI/Utils/Lists/ArrayList.cs
+++ AndroidUI/Utils/Lists/ArrayList.cs
@@ -1,153 +1,67 @@
-﻿using System.Collections;
-
-namespace AndroidUI.Utils.Lists
+﻿namespace AndroidUI.Utils.Lists
 {
-    class ArrayList<T> : IList<T>, System.Collections.IList, IReadOnlyList<T>
+    class ArrayList<T> : ListWrapper<T>
     {
-        T[] array = Array.Empty<T>();
-        int size;
-
-        ArrayList() {
+        public ArrayList()
+        {
         }
 
-        ArrayList(int capacity)
+        public ArrayList(int capacity) : base(capacity)
         {
-            EnsureCapacity(capacity);
+            Resize(capacity);
         }
 
-        ArrayList<T> Clone()
+        public ArrayList(IEnumerable<T> collection) : base(collection)
         {
-            ArrayList<T> values = new ArrayList<T>();
-            values.array = (T[])array.Clone();
-            values.size = size;
+        }
+
+        public override int Capacity
+        {
+            get => base.Capacity;
+            set
+            {
+                base.Capacity = value;
+                Resize(value);
+            }
+        }
+
+        public ArrayList<T> Clone()
+        {
+            ArrayList<T> values = new ArrayList<T>(Capacity);
+            values.AddRange(this);
             return values;
         }
 
-        public T this[int index] { get => array[index]; set => array[index] = value; }
-        object IList.this[int index] { get => array[index]; set => array[index] = (T)value; }
-
-        public int Count => array.Length;
-
-        public bool IsReadOnly => false;
-
-        public bool IsFixedSize => false;
-
-        public bool IsSynchronized => false;
-
-        public object SyncRoot => this;
-
-        // TODO: increase capacity by 2
-        public int increaseSize()
+        public override int EnsureCapacity(int min)
         {
-            int old = size;
-            size++;
-            Array.Resize(ref array, size);
-            return old;
+            int newCap = base.EnsureCapacity(min);
+            Resize(newCap);
+            return newCap;
         }
 
-        // TODO: decrease capacity by 2
-        public void decreaseSize()
+        public void Resize(int capacity)
         {
-            size--;
-            Array.Resize(ref array, size);
-        }
-
-        public void EnsureCapacity(int capacity)
-        {
-            size = capacity;
-            Array.Resize(ref array, capacity);
-        }
-
-        public void Add(T item)
-        {
-            array[increaseSize()] = item;
-        }
-
-        public int Add(object value)
-        {
-            T item = (T)value;
-            int r = increaseSize();
-            array[size] = item;
-            return r;
-        }
-
-        public void Clear()
-        {
-            Array.Clear(array);
-        }
-
-        public bool Contains(T item)
-        {
-            return array.Contains(item);
-        }
-
-        public bool Contains(object value)
-        {
-            return array.Contains((T)value);
-        }
-
-        public void CopyTo(T[] array, int arrayIndex)
-        {
-            array.CopyTo(array, arrayIndex);
-        }
-
-        public void CopyTo(Array array, int index)
-        {
-            array.CopyTo(array, index);
-        }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            return (IEnumerator<T>)array.GetEnumerator();
-        }
-
-        public int IndexOf(T item)
-        {
-            return Array.IndexOf(array, item);
-        }
-
-        public int IndexOf(object value)
-        {
-            return Array.IndexOf(array, value);
-        }
-
-        public void Insert(int index, T item)
-        {
-            int oldSize = increaseSize();
-            if (index < oldSize)
+            if (capacity == 0)
             {
-                Array.Copy(array, index, array, index + 1, oldSize - index);
+                Clear();
             }
-            array[index] = item;
-        }
-
-        public void Insert(int index, object value)
-        {
-            Insert(index, (T)value);
-        }
-
-        public bool Remove(T item)
-        {
-            int index = IndexOf(item);
-            if (index == -1) return false;
-            Array.Copy(array, index+1, array, index - 1, size - index);
-            decreaseSize();
-            return true;
-        }
-
-        public void Remove(object value)
-        {
-            Remove((T)value);
-        }
-
-        public void RemoveAt(int index)
-        {
-            Array.Copy(array, index + 1, array, index - 1, size - index);
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return array.GetEnumerator();
+            else
+            {
+                if (capacity > Count)
+                {
+                    while (Count < capacity)
+                    {
+                        Add(default(T));
+                    }
+                }
+                else if (capacity < Count)
+                {
+                    while (Count > capacity)
+                    {
+                        RemoveAt(Count - 1);
+                    }
+                }
+            }
         }
     }
 }
