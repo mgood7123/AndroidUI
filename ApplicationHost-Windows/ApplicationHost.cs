91e0a74d803201750cfedf9fb9f577440c43e638
mgood7123 <smallville7123@gmail.com>
2022-08-03 22:54:20 +1000

    simplify and improve exception handler

--- ApplicationHost-Windows/ApplicationHost.cs
+++ ApplicationHost-Windows/ApplicationHost.cs
@@ -32,31 +32,22 @@ namespace AndroidUI.Hosts.Windows
             Controls.Add(skia);
         }
 
+        private static void ShowException(string title, Exception e)
+        {
+            DialogResult result = DialogResult.Cancel;
+            result = MessageBox.Show("Do you want to exit?\n\nAn Exception has occured:\n\n" + e, title, MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+            if (result == DialogResult.Yes)
+            {
+                Log.d("AndroidUI Application Host", "\n-------- ERROR --------\n\n" + e.ToString() + "\n\n-------- ERROR --------\n");
+                Environment.Exit(-1);
+            }
+        }
+
         // Handle the UI exceptions by showing a dialog box, and asking the user whether
         // or not they wish to abort execution.
         internal static void UIThreadException(object sender, ThreadExceptionEventArgs t)
         {
-            DialogResult result = DialogResult.Cancel;
-            try
-            {
-                result = ShowThreadExceptionDialog("Windows Forms Error", t.Exception);
-            }
-            catch
-            {
-                try
-                {
-                    MessageBox.Show("Fatal Windows Forms Error",
-                        "Fatal Windows Forms Error", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop);
-                }
-                finally
-                {
-                    Application.Exit();
-                }
-            }
-
-            // Exits the program when the user clicks Abort.
-            if (result == DialogResult.Abort)
-                Application.Exit();
+            ShowException("UI Exception", t.Exception);
         }
 
         // Handle the UI exceptions by showing a dialog box, and asking the user whether
@@ -65,46 +56,7 @@ namespace AndroidUI.Hosts.Windows
         // log the event, and inform the user about it.
         internal static void NonUIThreadException(object sender, UnhandledExceptionEventArgs e)
         {
-            try
-            {
-                Exception ex = (Exception)e.ExceptionObject;
-                string errorMsg = "An application error occurred. Please contact the adminstrator " +
-                    "with the following information:\n\n";
-
-                // Since we can't prevent the app from terminating, log this to the event log.
-                if (!EventLog.SourceExists("ThreadException"))
-                {
-                    EventLog.CreateEventSource("ThreadException", "Application");
-                }
-
-                // Create an EventLog instance and assign its source.
-                EventLog myLog = new();
-                myLog.Source = "ThreadException";
-                myLog.WriteEntry(errorMsg + ex.Message + "\n\nStack Trace:\n" + ex.StackTrace);
-            }
-            catch (Exception exc)
-            {
-                try
-                {
-                    MessageBox.Show("Fatal Non-UI Error",
-                        "Fatal Non-UI Error. Could not write the error to the event log. Reason: "
-                        + exc.Message, MessageBoxButtons.OK, MessageBoxIcon.Stop);
-                }
-                finally
-                {
-                    Application.Exit();
-                }
-            }
-        }
-
-        // Creates the error message and displays it.
-        private static DialogResult ShowThreadExceptionDialog(string title, Exception e)
-        {
-            string errorMsg = "An application error occurred. Please contact the adminstrator " +
-                "with the following information:\n\n";
-            errorMsg = errorMsg + e.Message + "\n\nStack Trace:\n" + e.StackTrace;
-            return MessageBox.Show(errorMsg, title, MessageBoxButtons.AbortRetryIgnore,
-                MessageBoxIcon.Stop);
+            ShowException("Non-UI Exception", (Exception)e.ExceptionObject);
         }
 
         public static void InstallHandlers()
