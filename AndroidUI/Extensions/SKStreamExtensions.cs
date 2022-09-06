b36dcf9fda6ced1eedadeef17b98c52ac8f1aa32
Matthew Good <smallville7123@gmail.com>
2022-09-06 17:48:34 +1000

    extend SKStream a bit

---
+++ AndroidUI/Extensions/SKStreamExtensions.cs
@@ -0,0 +1,20 @@
+﻿using SkiaSharp;
+
+namespace AndroidUI.Extensions
+{
+    public static class SKStreamExtensions
+    {
+        public static bool ReadFloat(this SKStream stream, out float buffer)
+        {
+            buffer = 0.0f;
+            uint tmp;
+            if (!stream.ReadUInt32(out tmp))
+            {
+                return false;
+            }
+
+            buffer = tmp.BitsToFloat();
+            return true;
+        }
+    }
+}
